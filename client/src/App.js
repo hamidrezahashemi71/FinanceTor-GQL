190750cc5590e87ad48dbb69e824952f5e9e1954
hamidrezahashemi71 <hh7192@gmail.com>
2022-09-06 14:03:00 +0430

    boiler plate and packages

---
+++ client/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <div className='underline text-4xl'>Hello Financetor!</div>;
+}
+
+export default App;
---
+++ client/src/config/constants.js
@@ -0,0 +1,2 @@
+export const endpoint = "http://localhost:80/graphql";
+export const siteTitle = "FinanceTor";
